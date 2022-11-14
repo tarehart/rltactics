513f2a65d5cb7e55ea72f2b1c20198e6c78e22fe
Tyler Arehart <tarehart@gmail.com>
2022-11-13 16:44:14 -0800

    Making changes recommended by amplify vue setup tutorial.
    https://docs.amplify.aws/start/getting-started/setup/q/integration/vue

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { Amplify } from 'aws-amplify';
+import awsExports from './aws-exports';
 
 import './assets/main.css'
 
+Amplify.configure(awsExports);
 const app = createApp(App)
 
 app.use(router)
